cd14856ce2c61f1def63d87f4020376b6fa48b03
preciouStar <qkrtjdwls605@naver.com>
2020-11-12 11:05:34 +0900

    Add isCurrentWeekofMonth function

--- src/GetWeekOfMonth.js
+++ src/GetWeekOfMonth.js
@@ -3,7 +3,6 @@
  * 0: 1주차
  * 1: 2주차
  * 2: 3주차
- * etc
  */
 Date.prototype.getWeekOfMonth = function() {
   let firstWeekday =
@@ -13,13 +12,44 @@ Date.prototype.getWeekOfMonth = function() {
   return Math.floor(offsetDate / 7);
 };
 
-console.log(new Date("2020-09-27").getWeekOfMonth()); // 3
-console.log(new Date("2020-09-28").getWeekOfMonth()); // 4
-console.log(new Date("2020-10-01").getWeekOfMonth()); // 0
-console.log(new Date("2020-10-04").getWeekOfMonth()); // 0
-console.log(new Date("2020-10-05").getWeekOfMonth()); // 1
+// console.log(new Date("2020-09-27").getWeekOfMonth()); // 3
+// console.log(new Date("2020-09-28").getWeekOfMonth()); // 4
+// console.log(new Date("2020-10-01").getWeekOfMonth()); // 0
+// console.log(new Date("2020-10-04").getWeekOfMonth()); // 0
+// console.log(new Date("2020-10-05").getWeekOfMonth()); // 1
 
-console.log(new Date("2020-11-01").getWeekOfMonth()); // 0
-console.log(new Date("2020-11-02").getWeekOfMonth()); // 1
-console.log(new Date("2020-11-29").getWeekOfMonth()); // 4
-console.log(new Date("2020-11-30").getWeekOfMonth()); // 5
+// console.log(new Date("2020-11-01").getWeekOfMonth()); // 0
+// console.log(new Date("2020-11-02").getWeekOfMonth()); // 1
+// console.log(new Date("2020-11-29").getWeekOfMonth()); // 4
+// console.log(new Date("2020-11-30").getWeekOfMonth()); // 5
+
+/**
+ * 입력된 date가 오늘 기준으로 같은 주(week)인지 판단하여 return boolean
+ * @param {*} date
+ */
+function isCurrentWeekOfMonth(date) {
+  const isDateInstance = date instanceof Date;
+  const now = new Date();
+
+  if (
+    !date ||
+    !isDateInstance ||
+    now.getFullYear() !== date.getFullYear() ||
+    now.getMonth() !== date.getMonth() ||
+    now.getWeekOfMonth() !== date.getWeekOfMonth()
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(isCurrentWeekOfMonth(new Date("2020-11-08")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-09")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-10")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-11")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-12")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-13")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-14")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-15")));
+console.log(isCurrentWeekOfMonth(new Date("2020-11-16")));
