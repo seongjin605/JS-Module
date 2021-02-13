5053ab3a9dd48b1b8619f590e370fbf20f2cdb22
preciouStar <qkrtjdwls605@naver.com>
2021-02-13 23:05:35 +0900

    Add symbol

--- src/RepackagingCondition.js
+++ src/RepackagingCondition.js
@@ -10,3 +10,29 @@ const isRepackaging = () => {
 
 const result = isRepackaging();
 console.log('🚀 ~ result', result);
+
+const isValidItemType = () => {
+  const id = 'P540-2420-REFFOLD5GRJF';
+  const splitedArr = id.split('-');
+  const [first = '', second = '', third = ''] = splitedArr;
+
+  const appleValue = 10000;
+  const melonValue = 29000;
+  const peachValue = 50000;
+
+  const DiscountType = (() => {
+    const APPLE = Symbol.for('APPLE');
+    return {
+      [APPLE]: appleValue
+    };
+  })();
+  console.log('DiscountType:', DiscountType[Symbol.for('APPLE')]);
+
+  const itemType = (third, DAXItemType) => {
+    return third.toUpperCase().startsWith(DAXItemType, 0);
+  };
+
+  console.log('🔥 itemType: ', itemType(third, 'REF'));
+};
+
+isValidItemType();
