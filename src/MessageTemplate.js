1f338818e5d672d9a3583c2ee3f4cea0a835da02
preciouStar <qkrtjdwls605@naver.com>
2021-02-15 22:02:41 +0900

    Create MessageTemplate

--- src/MessageTemplate.js
+++ src/MessageTemplate.js
@@ -1,22 +1,29 @@
 const createMessageTemplate = () => {
   const str = '{tail}, {KK}, {namu}';
-  let message = `
+  let messageTemplate = `
   What is Lorem Ipsum?
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s {tail}, 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
   It has survived not only five centuries, {KK} but also the leap into electronic typesetting, remaining essentially unchanged. 
   It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-  and more recently with desktop publishing {namu} software like Aldus PageMaker including versions of Lorem Ipsum.
+  and more recently with desktop publishing {namu} software like Aldus PageMaker including versions of Lorem Ipsum.|
+  DO YOUR BEST.
   `;
   const params = str.split(',');
-  while (params.length > 0) {
-    const param = params.pop();
-    console.log('🚀 ~ createMessageTemplate ~ param', param);
-    if (param) {
-      message = message.replace(param, '🔥');
+  const templates = messageTemplate.split('|');
+
+  let message = '';
+
+  templates.forEach(val => {
+    const target = val.trim();
+    while (params.length > 0) {
+      const param = params.pop();
+      console.log(param);
+      message = target.replace(param.trim(), '🔥');
     }
-  }
+  });
+  console.log('result:', message);
   return message;
 };
 
