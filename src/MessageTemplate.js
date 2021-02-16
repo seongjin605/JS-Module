14157fa65461ac4467aabe71e4822cdc33602291
preciouStar <qkrtjdwls605@naver.com>
2021-02-16 18:10:26 +0900

    Update

--- src/MessageTemplate.js
+++ src/MessageTemplate.js
@@ -1,33 +1,68 @@
 const createMessageTemplate = () => {
-  const str = '{tail}|{KK}|{namu}';
+  const patternStr =
+    '{CustomerName}|{LastCreatedDate}|{ExternalReferenceNumber}|{notCompletedDocs}|{notSubmitted}';
 
   let messageTemplate = `
-  What is Lorem Ipsum?
-  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s {tail}, 
+  Hi Amazon!
+
+  LoremIpsum
+  
+  Dear {CustomerName}, {LastCreatedDate}is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, (👀 {ExternalReferenceNumber}) 
   when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, {KK} but also the leap into electronic typesetting, remaining essentially unchanged. 
-  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-  and more recently with desktop publishing {namu} software like Aldus PageMaker including versions of Lorem Ipsum.|
-  DO YOUR BEST. 💪 |
-  HEY WASSUP MAN 😀|
+  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+  
+  - TIKARA: {ExternalReferenceNumber}
+  - TOTORO: {LastCreatedDate}|
+  - ZOZO
+    * {notCompletedDocs}|
+  - KEKE
+    * {notSubmitted}|
+  
+    . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
   `;
-  const params = str.split('|');
-  const messageOptions = messageTemplate.split('|');
-  console.log('------------------------------------------------------------');
-  console.log('messageOptions length:', messageOptions.length);
-  console.log('🚀  0번째 인덱스가 기준:', messageOptions[0]);
-  console.log('🚀  1번째 인덱스 옵션:', messageOptions[1]);
-  console.log('🚀  1번째 인덱스 옵션:', messageOptions[2]);
-  console.log('------------------------------------------------------------');
 
   const result = [];
-  while (params.length > 0) {
-    const param = params.pop().trim();
-    messageOptions[0] = messageOptions[0].replace(param, '🔥');
+  const regexPatterns = patternStr.split('|');
+
+  const fillTemplate = (templateString, templateVars) => {
+    return new Function('return `' + templateString + '`;').call(templateVars);
+  };
+  const customer = {
+    CustomerName: 'jin.park'
+  };
+  const documentByClaimId = {
+    LastCreatedDate: new Date()
+  };
+  const templateVars = {
+    CustomerName: customer.CustomerName,
+    LastCreatedDate: documentByClaimId.LastCreatedDate,
+    notCompletedDocs: ['🔥'],
+    notSubmitted: ['😀']
+  };
+
+  while (regexPatterns.length > 0) {
+    const pattern = regexPatterns.pop();
+
+    const parsedPatternStr = pattern.split('');
+    parsedPatternStr.shift();
+    parsedPatternStr.pop();
+
+    const regex = new RegExp(pattern, 'g');
+
+    console.log('parsedPatternStr:', parsedPatternStr.join(''));
+
+    const here = '${this.' + parsedPatternStr.join('') + '}';
+    const value = fillTemplate(here, templateVars);
+    messageTemplate = messageTemplate.replace(regex, value);
   }
 
-  result.push(messageOptions[0], messageOptions[2]);
+  const messageOptions = messageTemplate.split('|');
+
+  console.log('message size:', messageOptions.length);
+
+  result.push(messageOptions[0], messageOptions[1], messageOptions[2]);
 
   return result.join('');
 };
